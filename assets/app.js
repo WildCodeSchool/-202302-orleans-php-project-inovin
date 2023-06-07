9b99fa0786e2f150464804a976cb5dbe0a95f4b6
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2023-06-07 16:45:08 +0200

    S01US04_home

--- assets/app.js
+++ assets/app.js
@@ -11,5 +11,6 @@ require("bootstrap");
 // any CSS you import will output into a single css file (app.css in this case)
 import "./styles/app.scss";
 
+
 // start the Stimulus application
 import "./bootstrap";
