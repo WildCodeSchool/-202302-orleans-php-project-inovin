1d6d400400c44927766ddfe1057d38649f33ef14
S.K.V.666 <melvin.rossignoles@gmail.com>
2023-06-08 15:54:43 +0200

    generated crud and added style to index

---
+++ src/DataFixtures/SessionFixtures.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\DataFixtures;
+
+use DateTime;
+use Faker\Factory;
+use DateTimeInterface;
+use App\Entity\Session;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class SessionFixtures extends Fixture
+{
+
+    public const SESSIONS = [
+        'Découverte des cépages : Explorez les variétés de vins du monde',
+        'Vins et fromages : Une harmonie parfaite',
+        'Un voyage au cœur des vignobles : Les terroirs qui donnent vie aux vins',
+        'Dégustation à l\'aveugle : Développez vos sens et testez vos connaissances',
+        'Vins effervescents : Champagne, Prosecco et bien plus encore',
+        'Le monde du vin en une soirée : Une initiation complète',
+        'Accords mets et vins : Trouvez l\'équilibre parfait',
+        'Vins bio et naturels : Dégustez des vins respectueux de l\'environnement',
+        'Vins rares et prestigieux : Une expérience d\'exception',
+        'Découverte des vins du Nouveau Monde : Des saveurs exotiques à explorer',
+    ];
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        foreach (self::SESSIONS as $key => $sessionName) {
+            $session = new Session();
+            $openingDate = new DateTime();
+
+            $session->setName($sessionName);
+            $session->setOpeningDate($faker->dateTimeThisYear($openingDate));
+            $session->setDescription($faker->sentence(3));
+            $session->setClosed($faker->numberBetween(0, 1));
+            $manager->persist($session);
+            $this->addReference('session_' . $key, $session);
+        }
+
+        $manager->flush();
+    }
+}
