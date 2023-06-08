3b08c6920a8d2e1c6f30148034db906f43e3b07b
Mike Xiong <xiong.tchylon@gmail.com>
2023-06-08 13:53:32 +0200

    create admin page

---
+++ src/Controller/AdminHomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/admin', name: 'app_admin_')]
+class AdminHomeController extends AbstractController
+{
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'hello admin' => 'hello',
+        ]);
+    }
+}
