9c1b73a948d94f2b30920b38699bef463874c3b6
BENJAMIN-SAUSSAYE <saussaye.ipod@gmail>
2023-06-08 12:31:54 +0200

    fin implementation add sur cépage

--- src/Form/GrapeVarietyType.php
+++ src/Form/GrapeVarietyType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\GrapeVariety;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,17 @@ class GrapeVarietyType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-        ;
+            ->add(
+                'name',
+                TextType::class,
+                [
+                    'label' => 'Titre',
+                    'required' => true,
+                    'attr' => [
+                        'placeholder' => 'nom du cépage...',
+                    ],
+                ]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
