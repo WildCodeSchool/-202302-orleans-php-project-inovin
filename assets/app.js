43154cda16d34a2070f75e2fd361d1f704416bc2
BENJAMIN-SAUSSAYE <saussaye.ipod@gmail>
2023-06-14 16:11:08 +0200

    fin style button link partie cepage

--- assets/app.js
+++ assets/app.js
@@ -11,5 +11,8 @@ require("bootstrap");
 // any CSS you import will output into a single css file (app.css in this case)
 import "./styles/app.scss";
 
+//import bootstrap icons
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 // start the Stimulus application
 import "./bootstrap";
