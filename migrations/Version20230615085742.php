aa0158d41a67e65a65af33f872d12e22ac640448
BENJAMIN-SAUSSAYE <saussaye.ipod@gmail>
2023-06-15 11:14:38 +0200

    ajout d'une zone description pour le cépage

---
+++ migrations/Version20230615085742.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230615085742 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE grape_variety ADD descriptif LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE wine RENAME INDEX idx_560c646882cc410a TO IDX_560C6468ED00A18A');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE wine RENAME INDEX idx_560c6468ed00a18a TO IDX_560C646882CC410A');
+        $this->addSql('ALTER TABLE grape_variety DROP descriptif');
+    }
+}
