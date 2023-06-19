65fb987af11d149d16d01946f0768832ee159761
Mike Xiong <xiong.tchylon@gmail.com>
2023-06-19 22:28:34 +0200

    fixe PR

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Il existe déjà un compte avec cette adresse e-mail.')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -21,7 +21,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(length: 180, unique: true)]
     #[Assert\NotBlank]
-    #[Assert\Length(max: 100)]
+    #[Assert\Length(max: 180)]
     private ?string $email = null;
 
     #[ORM\Column]
@@ -33,14 +33,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    #[ORM\Column(length: 25)]
+    #[ORM\Column(length: 150)]
     #[Assert\NotBlank]
-    #[Assert\Length(max: 25)]
+    #[Assert\Length(max: 150)]
     private ?string $firstname = null;
 
-    #[ORM\Column(length: 25)]
+    #[ORM\Column(length: 150)]
     #[Assert\NotBlank]
-    #[Assert\Length(max: 25)]
+    #[Assert\Length(max: 150)]
     private ?string $lastname = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
@@ -51,16 +51,16 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[Assert\Length(max: 9)]
     private ?string $zipCode = null;
 
-    #[ORM\Column(length: 45)]
-    #[Assert\Length(max: 45)]
+    #[ORM\Column(length: 255)]
+    #[Assert\Length(max: 255)]
     private ?string $city = null;
 
     #[ORM\Column(length: 255)]
     #[Assert\Length(max: 255)]
     private ?string $address = null;
 
-    #[ORM\Column(length: 45)]
-    #[Assert\Length(max: 45)]
+    #[ORM\Column(length: 255)]
+    #[Assert\Length(max: 255)]
     private ?string $country = null;
 
     #[ORM\Column(type: 'boolean')]
