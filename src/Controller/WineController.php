d6488ab3f34000b53bacb9ebe0991d0e816ba10b
BENJAMIN-SAUSSAYE <saussaye.ipod@gmail>
2023-06-20 21:17:45 +0200

    fin test unitphp

--- src/Controller/WineController.php
+++ src/Controller/WineController.php
@@ -38,7 +38,7 @@ class WineController extends AbstractController
             return $this->redirectToRoute('app_wine_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('wine/new.html.twig', [
+        return $this->render('wine/new.html.twig', [
             'wine' => $wine,
             'form' => $form,
         ]);
@@ -64,7 +64,7 @@ class WineController extends AbstractController
             return $this->redirectToRoute('app_wine_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('wine/edit.html.twig', [
+        return $this->render('wine/edit.html.twig', [
             'wine' => $wine,
             'form' => $form,
         ]);
--- tests/Controller/RouteTest.php
+++ tests/Controller/RouteTest.php
@@ -11,14 +11,47 @@ class RouteTest extends WebTestCase
      */
     public function testPageIsSuccessFul(string $url): void
     {
+        // This calls KernelTestCase::bootKernel(), and creates a
+        // "client" that is acting as the browser
         $client = static::createClient();
-        //$client->setServerParameter('HTTP_HOST', 'https://127.0.0.1:8001/');
-        $crawler = $client->request('GET', $url);
+        // Request a specific page
+        $client->request('GET', $url);
+        //var_print($url);
         $this->assertResponseIsSuccessful('Impossible d\'accéder à l\'url : ' . $url);
     }
 
     public function urlProvider(): mixed
     {
+        $forbidenRoutes = ['/verify/email'];
+        $router = $this->getContainer()->get('router');
+        $collection = $router->getRouteCollection();
+        $allRoutes = $collection->all();
+
+        foreach ($allRoutes as $route => $params) {
+            $defaults = $params->getDefaults();
+            //check route concerne controller
+            if (isset($defaults['_controller'])) {
+                //get the path of the route
+                $path = $params->getPath();
+                $methods = $params->getMethods();
+                //replace parameters brakets by value 1 in the route.
+                $routeParameters = array("{id}", "{session}", "{vin}", "{login}", "{grape_variety}, {user}");
+                $pathRoute = str_replace($routeParameters, '1', $path);
+                //check route not contains parameters not replaced in route
+                if (!str_contains($pathRoute, '{')) {
+                    //skip route where access is for specific roles or only connected user
+                    if (!in_array($pathRoute, $forbidenRoutes)) {
+                        //check method GET is possible for the route
+                        if (in_array("GET", $methods)) {
+                            //return the route to test
+                            yield [$pathRoute];
+                        }
+                    }
+                }
+            }
+        }
+
+        /*
         yield ['/']; //home page
         yield ['/admin/']; //admin home page
         yield ['/contact']; //contact page
@@ -26,5 +59,6 @@ class RouteTest extends WebTestCase
         yield ['/session/']; //admin index séance
         yield ['/admin/cepage/']; //admin index cépage
         yield ['/vin/']; //admin index vin
+        */
     }
 }
