dbd2df912b9c4f5ba2585c6dfb667143539132a2
BENJAMIN-SAUSSAYE <saussaye.ipod@gmail>
2023-06-20 13:33:15 +0200

    fix double index

--- migrations/Version20230615081440.php
+++ migrations/Version20230615081440.php
@@ -23,7 +23,6 @@ final class Version20230615081440 extends AbstractMigration
         $this->addSql('CREATE TABLE session_wine (session_id INT NOT NULL, wine_id INT NOT NULL, INDEX IDX_907D6442613FECDF (session_id), INDEX IDX_907D644228A2BD76 (wine_id), PRIMARY KEY(session_id, wine_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE session_wine ADD CONSTRAINT FK_907D6442613FECDF FOREIGN KEY (session_id) REFERENCES session (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE session_wine ADD CONSTRAINT FK_907D644228A2BD76 FOREIGN KEY (wine_id) REFERENCES wine (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE wine RENAME INDEX idx_560c646882cc410a TO IDX_560C6468ED00A18A');
     }
 
     public function down(Schema $schema): void
@@ -32,6 +31,5 @@ final class Version20230615081440 extends AbstractMigration
         $this->addSql('ALTER TABLE session_wine DROP FOREIGN KEY FK_907D6442613FECDF');
         $this->addSql('ALTER TABLE session_wine DROP FOREIGN KEY FK_907D644228A2BD76');
         $this->addSql('DROP TABLE session_wine');
-        $this->addSql('ALTER TABLE wine RENAME INDEX idx_560c6468ed00a18a TO IDX_560C646882CC410A');
     }
 }
