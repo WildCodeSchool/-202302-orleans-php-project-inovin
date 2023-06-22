c8f6b83179a699a2805a764d1b110a8c6ebe2b43
BENJAMIN-SAUSSAYE <saussaye.ipod@gmail>
2023-06-22 19:43:35 +0200

    correcif boostrap import

--- assets/app.js
+++ assets/app.js
@@ -6,11 +6,21 @@ import "./bootstrap.js";
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+const $ = require("jquery");
+
 //import bootstrap bundle
-require("bootstrap");
+import * as bootstrap from "bootstrap";
+window.bootstrap = bootstrap;
 
 // any CSS you import will output into a single css file (app.css in this case)
 import "./styles/app.scss";
 
 // start the Stimulus application
 import "./bootstrap";
+
+const popoverTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="popover"]'
+);
+const popoverList = [...popoverTriggerList].map(
+    (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
