568232b09267469111306ffd49ce662e3d5ade61
BENJAMIN-SAUSSAYE <saussaye.ipod@gmail>
2023-06-26 12:39:14 +0200

    fix requestchange

--- tests/Controller/RouteTest.php
+++ tests/Controller/RouteTest.php
@@ -28,43 +28,15 @@ class RouteTest extends WebTestCase
         $this->assertResponseIsSuccessful('Impossible d\'accéder à l\'url : ' . $url);
     }
 
-    public function urlProvider(): array
+    public function urlProvider(): mixed
     {
-        return $this->getAppRoutes();
-    }
-
-    private function getAppRoutes(): array
-    {
-        $myAppRoutes = [];
-        $forbidenRoutes = ['/verify/email'];
-        $router = $this->getContainer()->get('router');
-        $collection = $router->getRouteCollection();
-        $allRoutes = $collection->all();
-
-        foreach ($allRoutes as $route => $params) {
-            $defaults = $params->getDefaults();
-            //check route concerne controller
-            if (isset($defaults['_controller'])) {
-                //get the path of the route
-                $path = $params->getPath();
-                $methods = $params->getMethods();
-                //replace parameters brakets by value 1 in the route.
-                $routeParameters = array("{id}", "{session}", "{vin}", "{login}", "{grape_variety}, {user}");
-                $pathRoute = str_replace($routeParameters, '1', $path);
-                //check route not contains parameters not replaced in route
-                if (!str_contains($pathRoute, '{')) {
-                    //skip route where access is for specific roles or only connected user
-                    if (!in_array($pathRoute, $forbidenRoutes)) {
-                        //check method GET is possible for the route
-                        if (in_array("GET", $methods)) {
-                            //add the route to array for the test
-                            $myAppRoutes[] = [$pathRoute];
-                        }
-                    }
-                }
-            }
-        }
-
-        return $myAppRoutes;
+        yield ['/']; //home page
+        yield ['/admin/'];
+        yield ['/session/'];
+        yield ['/session/new'];
+        yield ['/vin/'];
+        yield ['/vin/new'];
+        yield ['/admin/cepage/'];
+        yield ['/admin/cepage/new'];
     }
 }
