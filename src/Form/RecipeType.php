dd777afae5c43b20950d6a49115caac9b4285743
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2023-06-26 12:34:28 +0200

    update recette

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -2,69 +2,19 @@
 
 namespace App\Form;
 
-use App\Entity\User;
 use App\Entity\Recipe;
-use App\Entity\Session;
-use Doctrine\DBAL\Types\TextType;
-use App\Repository\UserRepository;
-use App\Repository\SessionRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class RecipeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add(
-                'name',
-                TextType::class,
-                [
-                    'label' => 'Nom de la recette',
-                    'attr' => [
-                        'placeholder' => 'Thomas',
-                    ],
-                ]
-            )
-            ->add(
-                'session_rate',
-                NumberType::class,
-                [
-                    'label' => 'Note de la recette',
-                    'attr' => [
-                        'placeholder' => '14',
-                    ],
-                ]
-            )
-            ->add(
-                'session',
-                EntityType::class,
-                [
-                    'class' => Session::class,
-                    'query_builder' => function (SessionRepository $sr) {
-                        return $sr->createQueryBuilder('s')
-                            ->orderBy('s.name', 'ASC');
-                    },
-                    'choice_label' => 'name',
-                    'by_reference' => true,
-                ]
-            )
-            ->add(
-                'user',
-                EntityType::class,
-                [
-                    'class' => User::class,
-                    'query_builder' => function (UserRepository $ur) {
-                        return $ur->createQueryBuilder('u')
-                            ->orderBy('u.firstname', 'ASC');
-                    },
-                    'choice_label' => 'firstname',
-                    'by_reference' => true,
-                ]
-            );
+            ->add('session')
+            ->add('user')
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
