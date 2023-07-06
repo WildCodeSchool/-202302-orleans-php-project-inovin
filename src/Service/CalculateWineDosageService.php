9a59c791cc1733065547bcc64fe33d4e751c62d9
BENJAMIN-SAUSSAYE <saussaye.ipod@gmail>
2023-07-06 07:52:56 +0200

    en attente

--- src/Service/CalculateWineDosageService.php
+++ src/Service/CalculateWineDosageService.php
@@ -4,27 +4,51 @@ namespace App\Service;
 
 use App\Entity\Recipe;
 use App\Entity\TastingSheet;
+use App\Repository\TastingSheetRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\SecurityBundle\Security;
 
 #[IsGranted('ROLE_USER')]
 class CalculateWineDosageService
 {
-    public function calculate(Recipe $recipe): array
+    public function __construct(private TastingSheetRepository $tastingShRepository)
+    {
+    }
+    public function calculate(Recipe $recipe): Recipe
+    {
+        $orderedItems = $this->orderedElementsByAverage($recipe);
+
+        foreach ($orderedItems as $key => $itemOrdered) {
+            foreach ($recipe->getTastingSheet() as $itemTastingSheet) {
+                if ($itemOrdered['tastingSheet'] === $itemTastingSheet->getId()) {
+                    switch ($key) {
+                        case 0:
+                            //$itemTastingSheet->setDosage(150);
+                            $this->tastingShRepository->save($itemOrdered, true);
+                            break;
+                    }
+                }
+            }
+        }
+
+        return $recipe;
+    }
+    private function orderedElementsByAverage(Recipe $recipe): array
     {
         $result = [];
-
         foreach ($recipe->getTastingSheet() as $itemTastingSheet) {
             $result[] = [
                 'tastingSheet' => $itemTastingSheet->getId(),
-                'wine' => $itemTastingSheet->getWine()->getId(),
                 'average' => $this->getAverage($itemTastingSheet)
             ];
         }
 
+        $col = array_column($result, "average");
+
+        array_multisort($col, SORT_ASC, $result);
+
         return $result;
     }
-
     private function getAverage(TastingSheet $tastingSheet): float
     {
         return (($tastingSheet->getSmell() ?? 0) +
