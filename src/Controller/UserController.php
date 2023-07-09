535462909f6a0efc05efaf51e2f660bd231ff3cc
Mike Xiong <xiong.tchylon@gmail.com>
2023-07-09 17:12:35 +0200

    secure profile page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,6 +7,7 @@ use App\Form\UserType;
 use App\Form\RegistrationFormType;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -20,8 +21,13 @@ class UserController extends AbstractController
     public function showProfile(
         Request $request,
         User $user,
-        UserRepository $userRepository
+        UserRepository $userRepository,
     ): Response {
+        $currentUser = $this->getUser();
+        if ($currentUser !== $user) {
+            throw $this->createAccessDeniedException("Accès refusé. Vous n'êtes pas autorisé à accéder à ce profil.");
+        }
+
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
