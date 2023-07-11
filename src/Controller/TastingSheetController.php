172aa80cc0b407e12ee0fdc3d89b0dbbe332455a
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2023-07-11 16:26:19 +0200

    test

--- src/Controller/TastingSheetController.php
+++ src/Controller/TastingSheetController.php
@@ -16,49 +16,49 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TastingSheetController extends AbstractController
 {
-    // #[IsGranted('ROLE_USER')]
-    // #[Route('/degustation/{recipe}', name: 'app_tasting_sheet')]
-    // public function index(Request $request, RecipeRepository $recipeRepository, Recipe $recipe): Response
-    // {
-    //     foreach ($recipe->getSession()->getWines() as $wine) {
-    //         $tastingSheet = new TastingSheet();
-    //         $tastingSheet->setWine($wine);
-    //         $recipe->addTastingSheet($tastingSheet);
-    //     }
-    //     $form = $this->createForm(RecipeTastingSheetType::class, $recipe);
-    //     $form->handleRequest($request);
-    //     if ($form->isSubmitted()) {
-    //         $recipeRepository->save($recipe, true);
-    //         return $this->redirectToRoute('app_tasting_sheet_result', ['recipe' => $recipe->getId()]);
-    //     }
-    //     return $this->render('tasting_sheet/index.html.twig', [
-    //         'recipe' => $recipe,
-    //         'form' => $form,
-    //     ]);
-    // }
+    #[IsGranted('ROLE_USER')]
+    #[Route('/degustation/{recipe}', name: 'app_tasting_sheet')]
+    public function index(Request $request, RecipeRepository $recipeRepository, Recipe $recipe): Response
+    {
+        foreach ($recipe->getSession()->getWines() as $wine) {
+            $tastingSheet = new TastingSheet();
+            $tastingSheet->setWine($wine);
+            $recipe->addTastingSheet($tastingSheet);
+        }
+        $form = $this->createForm(RecipeTastingSheetType::class, $recipe);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $recipeRepository->save($recipe, true);
+            return $this->redirectToRoute('app_tasting_sheet_result', ['recipe' => $recipe->getId()]);
+        }
+        return $this->render('tasting_sheet/index.html.twig', [
+            'recipe' => $recipe,
+            'form' => $form,
+        ]);
+    }
 
-    // #[IsGranted('ROLE_USER')]
-    // #[Route('degustation/{recipe}/resultat', name: 'app_tasting_sheet_result')]
-    // public function result(
-    //     Recipe $recipe,
-    //     CalculateWineDosageService $calculWineDosageSrv,
-    //     TastingSheetRepository $tastingRepository
-    // ): Response {
+    #[IsGranted('ROLE_USER')]
+    #[Route('degustation/{recipe}/resultat', name: 'app_tasting_sheet_result')]
+    public function result(
+        Recipe $recipe,
+        CalculateWineDosageService $calculWineDosageSrv,
+        TastingSheetRepository $tastingRepository
+    ): Response {
 
-    //     $resultDosages = $calculWineDosageSrv->calculate($recipe);
+        $resultDosages = $calculWineDosageSrv->calculate($recipe);
 
-    //     foreach ($resultDosages as $itemDosage) {
-    //         foreach ($recipe->getTastingSheet() as $itemTastingSheet) {
-    //             if ($itemTastingSheet->getId() === $itemDosage['tastingSheet']->getId()) {
-    //                 $itemTastingSheet->setDosage($itemDosage['dosage']);
-    //                 $tastingRepository->save($itemTastingSheet, true);
-    //                 continue;
-    //             }
-    //         }
-    //     }
+        foreach ($resultDosages as $itemDosage) {
+            foreach ($recipe->getTastingSheet() as $itemTastingSheet) {
+                if ($itemTastingSheet->getId() === $itemDosage['tastingSheet']->getId()) {
+                    $itemTastingSheet->setDosage($itemDosage['dosage']);
+                    $tastingRepository->save($itemTastingSheet, true);
+                    continue;
+                }
+            }
+        }
 
-    //     return $this->render('tasting_sheet/resultTastingSheet.html.twig', [
-    //         'recipe' => $recipe,
-    //     ]);
-    // }
+        return $this->render('tasting_sheet/resultTastingSheet.html.twig', [
+            'recipe' => $recipe,
+        ]);
+    }
 }
