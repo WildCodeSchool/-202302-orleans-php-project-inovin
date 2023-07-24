cfb0d14e2e446880ffd12f261b354e27186dfc5c
BENJAMIN-SAUSSAYE <saussaye.ipod@gmail>
2023-07-24 11:24:22 +0200

    fix change request

--- assets/rangeAnimation.js
+++ assets/rangeAnimation.js
@@ -1,8 +1,8 @@
 let ranges = document.querySelectorAll("input[type=range]");
-for (var i = 0; i < ranges.length; i++) {
+for (let i = 0; i < ranges.length; i++) {
     ranges[i].addEventListener("change", function () {
         let total = 0;
-        for (var r = 0; r < ranges.length; r++) {
+        for (let r = 0; r < ranges.length; r++) {
             total += Number(ranges[r].value);
         }
         let containerTotal = document.getElementById("totalDosage");
